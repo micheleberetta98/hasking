4f894aa457af40cf657e884028aada6b45a870ce
Michele Beretta <beretta.m98@gmail.com>
2021-03-12 20:44:21 +0100

    nicer code building

--- src/Code.hs
+++ src/Code.hs
@@ -7,7 +7,6 @@ import           Data.List     (foldl')
 import qualified Data.Map      as M
 import           Instruction   (Instruction (Control, Step, TapeValue),
                                 parseInstruction, stateId, valueSymbol)
-import           Parser
 import           Tape          (Tape)
 import qualified Tape          as T
 import           TuringMachine (FromState, State (State), StateList, ToState,
@@ -19,6 +18,9 @@ import           TuringMachine (FromState, State (State), StateList, ToState,
 
 -- | The raw lines that define the behaviour of the machine, each line defines an `Instruction`
 type Code = String
+
+type From = FromState String String
+type To = ToState String String
 
 -- | The whole machine specification, derived from a piece of code
 data MachineCode = MachineCode
@@ -41,23 +43,38 @@ empty = MachineCode M.empty (State "") [] T.empty
 parseCode :: Code -> Maybe MachineCode
 parseCode = foldl' f (Just empty) . lines
   where
-    f code line = updateCode code (parseInstruction line)
+    f code line = updateCode (parseInstruction line) code
 
-updateCode :: Maybe MachineCode -> Maybe Instruction -> Maybe MachineCode
-updateCode Nothing _          = Nothing
-updateCode _ Nothing          = Nothing
-updateCode (Just c) (Just s@Step {}) =
-  let (from, to) = convertStep s
-  in Just $ c{ transitions = M.insert from to (transitions c) }
-updateCode (Just c) (Just s@(Control "BEGIN" state)) =
-  Just $ c{ initialState = stateId $ head state }
-updateCode (Just c) (Just s@(Control "FINAL" states)) =
-  Just $ c{ finalStates = map stateId states }
-updateCode (Just c) (Just s@(TapeValue tape)) =
-  Just $ c{ initialTape = T.fromList tape }
+-- | Updates the machine code given a single instruction
+updateCode :: Maybe Instruction -> Maybe MachineCode -> Maybe MachineCode
+updateCode (Just s@Step {})             = withTransition $ convertStep s
+updateCode (Just c@(Control "BEGIN" s)) = withInitialState $ map stateId s
+updateCode (Just c@(Control "FINAL" s)) = withFinalStates $ map stateId s
+updateCode (Just t@(TapeValue tape))    = withTape tape
+updateCode _                            = const Nothing
+
+-- | Inserts a new transitions into the existing ones
+withTransition :: (From, To) -> Maybe MachineCode -> Maybe MachineCode
+withTransition (from, to) (Just c) = Just $ c{ transitions = M.insert from to (transitions c) }
+withTransition _  _                = Nothing
+
+-- | Updates the initial state of the machine code
+withInitialState :: [State String] -> Maybe MachineCode -> Maybe MachineCode
+withInitialState [state] (Just c) = Just $ c{ initialState = state }
+withInitialState _ _              = Nothing
+
+-- | Updates the final states of the machine code
+withFinalStates :: StateList String -> Maybe MachineCode -> Maybe MachineCode
+withFinalStates states (Just c) = Just $ c{ finalStates = states }
+withFinalStates _ _             = Nothing
+
+-- | Updates the initial tape value
+withTape :: [String] -> Maybe MachineCode -> Maybe MachineCode
+withTape tape (Just c) = Just $ c{ initialTape = T.fromList tape }
+withTape _ _           = Nothing
 
 -- | It converts a `Step` instruction into a tuple made of `FromState` and `ToState`
-convertStep :: Instruction -> (FromState String String, ToState String String)
+convertStep :: Instruction -> (From, To)
 convertStep (Step s1 v1 s2 v2 d) = (from, to)
   where
     from = (stateId s1, valueSymbol v1)
