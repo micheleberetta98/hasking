9ed815c6817c14ed7e10110214e281802df5c7b0
micheleberetta98 <beretta.m98@gmail.com>
2021-05-17 09:33:00 +0200

    :white_check_mark: added some tests

--- test/CodeSpec.hs
+++ test/CodeSpec.hs
@@ -9,10 +9,15 @@ import           TuringMachine
 codeTests :: SpecWith ()
 codeTests = describe "Code" $ do
   it "should parse a correct code" $ do
-    let Right code = fromCode "; A comment line\n{0 0}\n[BEGIN s1] ; This is the initial state\n[FINAL s2]\n(s1 0 s2 . S)\n      ; Another comment"
+    let
+      Right code = fromCode "; A comment line\n{0 . 1}\n[BEGIN s1] ; This is the initial state\n[FINAL s2]\n(s1 0 s2 . S)\n      ; Another comment"
+      tape = initialTape code
     initialState code `shouldBe` State "s1"
     finalStates code `shouldBe` [State "s2"]
     transitions code `shouldBe` M.fromList [((State "s1", Symbol "0"), (State "s2", Blank, S))]
+    value tape `shouldBe` Symbol "0"
+    value (move R tape) `shouldBe` Blank
+    value (move R (move R tape)) `shouldBe` Symbol "1"
 
   it "should not parse wrong code" $ do
     let
--- test/TuringMachineSpec.hs
+++ test/TuringMachineSpec.hs
@@ -7,11 +7,17 @@ import           TuringMachine
 
 turingMachineTests :: SpecWith ()
 turingMachineTests = describe "Turing Machine" $ do
-  it "should give back the tape when everything goes right" $ do
+  it "should give back the tape when everything goes right (1)" $ do
     let
       tape = fromList [Symbol 0, Symbol 0, Symbol 0, Symbol 0]
       result = machine t (State "A") [State "C"] tape
     toList <$> result `shouldBe` Right [Symbol 1, Symbol 1, Symbol 1, Symbol 1]
+
+  it "should give back the tape when everything goes right (2)" $ do
+    let
+      tape = fromList [Symbol 0, Blank, Symbol 1]
+      result = machine t (State "A") [State "C"] tape
+    toList <$> result `shouldBe` Right [Symbol 1]
 
   it "should give back Nothing when something goes wrong" $ do
     let
@@ -28,3 +34,9 @@ t = M.fromList
   , ((State "B", Symbol 1), (State "B", Symbol 1, L))
   ]
 
+t' = M.fromList
+  [ ((State "A", Symbol 0), (State "A", Blank, R))
+  , ((State "A", Blank),    (State "B", Blank, R))
+  , ((State "B", Symbol 1), (State "B", Blank, R))
+  , ((State "C", Blank),    (State "C", Symbol 1, S))
+  ]
