2504b35ed76a922e2fccaccb8835e8321093ad36
micheleberetta98 <beretta.m98@gmail.com>
2021-07-28 22:32:41 +0200

    :bookmark: bump breaking

--- app/Opts.hs
+++ app/Opts.hs
@@ -98,7 +98,7 @@ withInteractiveModeOn opts = return opts{ interactive = True }
 -- | Prints the version
 printVersion :: a -> IO b
 printVersion = const $ do
-  hPutStrLn stderr "1.3.2"
+  hPutStrLn stderr "2.0.0"
   exitSuccess
 
 -- | Prints the usage
