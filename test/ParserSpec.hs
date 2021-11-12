af234def35b4251de3d6ea2e5617f49816058453
micheleberetta98 <beretta.m98@gmail.com>
2021-11-12 21:49:52 +0100

    :white_check_mark: added new parser tests

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -2,15 +2,17 @@
 
 module ParserSpec where
 
-import           Code
+import           Code.Internal
+import           Control.Monad.IO.Class
 import           Data.Char
 import           Data.Either
-import           Data.Text       (Text)
+import           Data.Text              (Text)
+import qualified Data.Text              as T
 import           Parser.Internal
-import           Pretty          (Pretty (pretty))
+import           Pretty                 (Pretty (pretty))
 import           Tape
 import           Test.Hspec
-import           Text.Megaparsec hiding (State)
+import           Text.Megaparsec        hiding (State)
 
 over :: Parsec e s a -> s -> Either (ParseErrorBundle s e) a
 p `over` input = parse p "" input
@@ -48,3 +50,39 @@ parserTests = describe "Parser" $ do
 
   it "parses whole tapes" $ do
     pretty <$> (parseTape `over` "(0 1 0 1)") `shouldBe` Right "0 1 0 1"
+
+  it "parses states" $ do
+    parseState `over` "q0" `shouldBe` Right (State "q0")
+    parseState `over` "1a" `shouldSatisfy` isLeft
+
+  it "parses simulate (i.e. tapes)" $ do
+    pretty <$> (parseSimulate `over` "(simulate-on (0 1 r #))") `shouldBe` Right "(simulate-on (0 1 r #))"
+    parseSimulate `over` "(simulate-on 0 1 r #)" `shouldSatisfy` isLeft
+    parseSimulate `over` "(simulateon (0 1 r #))" `shouldSatisfy` isLeft
+    parseSimulate `over` "(simulateon ([ ]))" `shouldSatisfy` isLeft
+
+  it "parses rules" $ do
+    parseRule `over` "(s  0 s  1 R)" `shouldBe` Right (Rule (State "s")  (Symbol "0") (State "s") (Symbol "1") R)
+    parseRule `over` "(q0 # q1 . S)" `shouldBe` Right (Rule (State "q0") (Symbol "#") (State "q1") Blank       S)
+    parseRule `over` "(s 0 s 1 T)" `shouldSatisfy` isLeft
+    parseRule `over` "(s 0 1 s R)" `shouldSatisfy` isLeft
+    parseRule `over` "(0 0 0 0 S)" `shouldSatisfy` isLeft
+
+  it "parses machine definitions" $ do
+    let def = "(machine\n\
+              \ ; Ignoring comments\n\
+              \  (initial s)\n\
+              \  (finals (f))\n\
+              \  (rules\n\
+              \    ((s 0 s 1 R)\n\
+              \     (s . x . L)\n\
+              \     (x 1 x 1 L)\n\
+              \     (x . f . R))))\n\
+              \"
+    parseDefinition `over` def `shouldBe` Right (
+      Definition (State "s") [State "f"]
+        [ Rule (State "s") (Symbol "0") (State "s") (Symbol "1") R
+        , Rule (State "s") Blank        (State "x") Blank        L
+        , Rule (State "x") (Symbol "1") (State "x") (Symbol "1") L
+        , Rule (State "x") Blank        (State "f") Blank        R
+        ])
